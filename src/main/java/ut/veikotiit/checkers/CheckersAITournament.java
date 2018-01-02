9c87ed644000c39f2eecada2d0bbdb68caf19752
Tiit Oja <tiit.oja@gmail.com>
2018-01-02 19:19:16 +0200

    Aggregate results from AI games for every scorer implementation
    Takes all scorer 2-permutations out of all scorers and plays 100 games with each

---
+++ src/main/java/ut/veikotiit/checkers/CheckersAITournament.java
@@ -0,0 +1,67 @@
+package ut.veikotiit.checkers;
+
+import org.apache.commons.collections4.MultiSet;
+import org.apache.commons.collections4.multiset.HashMultiSet;
+import ut.veikotiit.checkers.game.AutomatedNoUIGame;
+import ut.veikotiit.checkers.game.GameResult;
+import ut.veikotiit.checkers.scorer.BitBoardScorer;
+import ut.veikotiit.checkers.scorer.DefaultBitBoardScorer;
+import ut.veikotiit.checkers.scorer.WeightedBitBoardScorer;
+import ut.veikotiit.checkers.util.AIScorerPair;
+
+import java.util.*;
+
+public class CheckersAITournament {
+
+  /**
+   * Determine how many games each AI scorer combination will play
+   */
+  private static final int GAME_COUNT = 100;
+
+  // TODO: 2.01.18 Dependency injection?
+  private static final List<BitBoardScorer> scorers = Arrays.asList(
+          DefaultBitBoardScorer.getInstance(),
+          WeightedBitBoardScorer.getInstance()
+  );
+
+  private final Map<AIScorerPair, MultiSet<GameResult>> results = new HashMap<>();
+
+  public static void main(String[] args) {
+    CheckersAITournament checkersAITournament = new CheckersAITournament();
+
+    for (AIScorerPair aiScorerPair : checkersAITournament.getAllScorerPairCombinations()) {
+      for (int i = 0; i < GAME_COUNT; i++) {
+        checkersAITournament.results.computeIfAbsent(aiScorerPair, x -> new HashMultiSet<>())
+                .add(new AutomatedNoUIGame(aiScorerPair.getWhite(), aiScorerPair.getBlack()).play());
+      }
+    }
+    checkersAITournament.prettyPrintResults();
+  }
+
+  private List<AIScorerPair> getAllScorerPairCombinations() {
+    List<AIScorerPair> combinations = new ArrayList<>();
+
+    for (int i = 0; i < scorers.size() - 1; i++) {
+      for (int j = i + 1; j < scorers.size(); j++) {
+        AIScorerPair aiScorerPair = AIScorerPair.of(scorers.get(i), scorers.get(j));
+        combinations.add(aiScorerPair);
+        combinations.add(AIScorerPair.flip(aiScorerPair));
+      }
+    }
+    return combinations;
+  }
+
+  private void prettyPrintResults() {
+    for (Map.Entry<AIScorerPair, MultiSet<GameResult>> resultPair : results.entrySet()) {
+      AIScorerPair aiScorerPair = resultPair.getKey();
+      MultiSet<GameResult> results = resultPair.getValue();
+      System.out.format("Black: %s - White: %s %10s\n",
+              aiScorerPair.getBlack().getClass().getSimpleName(),
+              aiScorerPair.getWhite().getClass().getSimpleName(),
+              results
+      );
+    }
+  }
+
+
+}
---
+++ src/main/java/ut/veikotiit/checkers/util/AIScorerPair.java
@@ -0,0 +1,29 @@
+package ut.veikotiit.checkers.util;
+
+import ut.veikotiit.checkers.scorer.BitBoardScorer;
+
+public class AIScorerPair {
+  private final BitBoardScorer white;
+  private final BitBoardScorer black;
+
+  private AIScorerPair(BitBoardScorer white, BitBoardScorer black) {
+    this.white = white;
+    this.black = black;
+  }
+
+  public static AIScorerPair of(BitBoardScorer white, BitBoardScorer black) {
+    return new AIScorerPair(white, black);
+  }
+
+  public static AIScorerPair flip(AIScorerPair aiScorerPair) {
+    return new AIScorerPair(aiScorerPair.black, aiScorerPair.white);
+  }
+
+  public BitBoardScorer getWhite() {
+    return white;
+  }
+
+  public BitBoardScorer getBlack() {
+    return black;
+  }
+}
