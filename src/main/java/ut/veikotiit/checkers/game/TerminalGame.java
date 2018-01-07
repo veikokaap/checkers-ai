6278117b0e2fcdc1e567f77b1b03d9ead8c7a547
Veiko Kääp <veikokaap@gmail.com>
2018-01-07 16:53:26 +0200

    Increase terminal game timeGiven.

--- src/main/java/ut/veikotiit/checkers/game/TerminalGame.java
+++ src/main/java/ut/veikotiit/checkers/game/TerminalGame.java
@@ -144,7 +144,7 @@ public class TerminalGame implements Game {
   }
 
   private GameResult startGame() throws InterruptedException, IOException {
-    IterativeDeepeningSearcher searcher = new IterativeDeepeningSearcher(100, 1000);
+    IterativeDeepeningSearcher searcher = new IterativeDeepeningSearcher(100, 3000);
     while (true) {
       if (move(searcher)) {
         if (bitBoard.getNextColor() == Color.WHITE) {
